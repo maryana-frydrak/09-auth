e5f309ac4d1f43adfe5a71ec4b94e690e5375cca
maryana-frydrak <maryanafrydrak@gmail.com>
2026-06-16 23:59:45 +0300

    fix api.ts

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
-
 export const nextServer = axios.create({
-  baseURL,
+  baseURL: "https://09-auth-indol-one.vercel.app/",
   withCredentials: true,
 });
