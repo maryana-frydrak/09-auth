8cd408c2c26b527a549b381aa258311966200854
maryana-frydrak <maryanafrydrak@gmail.com>
2026-06-17 07:29:56 +0300

    fix lib/api/api.ts

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
 
+if (!baseURL) {
+  throw new Error("NEXT_PUBLIC_API_URL is not defined");
+}
 export const nextServer = axios.create({
-  baseURL,
+  baseURL: `${baseURL}/api`,
   withCredentials: true,
 });
